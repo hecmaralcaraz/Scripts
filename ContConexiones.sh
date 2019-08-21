0a45e14981c7e82da6b35909e5c7f84f73f178bc
Hèctor Martínez <46947878+hecmaralcaraz@users.noreply.github.com>
2019-08-21 12:34:31 +0200

    Update ContConexiones.sh

--- ContConexiones.sh
+++ ContConexiones.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Hector Martinez Alcaraz 12/08/2019
 
-# Script para contar conexiones a la IP definida en el primer parametro
+# Script para contar conexiones a la IP definida en el primer parametro y guardar un log con la hora.
 
 
 NConex=$(netstat -lan | awk '{print $5}' | awk -F \: '{print $1}' | grep $1 | wc -l)
@@ -9,7 +9,7 @@ hora=$(date +%k:%M:%S)
 
 if [ $1 ]; then
 
-    echo $hora $NConex >>  /home/weblogic/ContarConexiones/ContadorConexiones.txt
+    echo $hora $NConex >>  log.txt
 else
     echo "Introduce una ip en el primer parametro"
 fi
