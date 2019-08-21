b204c614aac12995355310f158f0ec82fd60d44a
Hèctor Martínez <46947878+hecmaralcaraz@users.noreply.github.com>
2019-08-21 12:31:22 +0200

    Create MayusMin.py

---
+++ MayusMin.py
@@ -0,0 +1,45 @@
+# Python 3
+# Hèctor Martínez 21/08/2019
+
+# Este programa sirve para transformar una cadena de caracteres de mayus a minus y viceversa.
+# Requisitos: Se necesita la libreria pyperclip
+
+# Imports
+
+import pyperclip
+
+
+# Funciones
+
+def MayMin():
+    '''Transforma un texto en mayuscúlas a minuscúlas'''
+    palabra = input('\nPor favor, escriba el texto a transformar.\n>')
+    return palabra.lower()
+
+def MinMay():
+    '''Transforma un texto en minuscúlas a mayuscúlas'''
+    palabra = input('\nPor favor, escriba el texto a transformar.\n>')
+    return palabra.upper()
+
+
+# Main
+
+while True:
+    print('Buenos días señor.')
+    print('¿Que desea transformar hoy?\n')
+    print('1. De mayuscúlas a minuscúlas')
+    print('2. De minuscúlas a mayuscúlas')
+    opcion = input('\n>')
+    if opcion == '1':
+        texto = MayMin()
+        print('\n'+texto)  # imprime el texto transformado
+        break
+    elif opcion == '2':
+        texto = MinMay()
+        print('\n'+texto)  # imprime el texto transformado
+        break
+
+pyperclip.copy(texto)  # copia el texto al portapapeles
+
+print('\nEl texto se ha copiado al portapapeles.')
+exit = input('Presione Enter para salir...')
